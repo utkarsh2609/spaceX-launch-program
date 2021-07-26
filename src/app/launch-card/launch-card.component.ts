1b399209571c45907192c0774a605e373ee26ef6
utkarsh2609 <utkarsh.awasthi01@gmail.com>
2021-07-26 11:17:22 +0530

    setting up card component

--- src/app/launch-card/launch-card.component.ts
+++ src/app/launch-card/launch-card.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./launch-card.component.scss']
 })
 export class LaunchCardComponent implements OnInit {
+  dummyUrl = 'https://images.unsplash.com/photo-1516850228053-a807778c4e0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80'
 
   constructor() { }
 
