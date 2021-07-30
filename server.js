1d70dfd5e64ed14a32dcd60771f2309b1c1d5a4c
utkarsh2609 <utkarsh.awasthi01@gmail.com>
2021-07-30 18:01:26 +0530

    changes for deployment

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/space-x-assignment'));
+app.use(express.static(__dirname + '/dist/space-x-assignment'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/space-x-assignment/'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/space-x-assignment/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
