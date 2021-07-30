23c930fb11330cd026417ae17f6ae2573d5242c6
utkarsh2609 <utkarsh.awasthi01@gmail.com>
2021-07-30 20:11:54 +0530

    updating server file

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/space-x-assignment'));
+app.use(express.static('./dist/space-x-assignment'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/space-x-assignment/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/space-x-assignment/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainSectionComponent } from './main-section/main-section.component';
 
 const routes: Routes = [
-  {path: '', pathMatch:'full', redirectTo: 'home'},
-  {path: 'home', component: MainSectionComponent}
+  // {path: '', pathMatch:'full', redirectTo: 'home'},
+  // {path: 'home', component: MainSectionComponent}
 ];
 
 @NgModule({
